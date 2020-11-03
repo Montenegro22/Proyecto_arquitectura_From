aecd114d54b928458202bac86c0609f21c020efe
Natalia <montenegro.natismontenegro@hotmail.com>
2020-11-03 03:14:19 -0600

    funcion eliminar

--- js/Scrip.js
+++ js/Scrip.js
@@ -34,7 +34,6 @@ $("#BTN_CREAR").click(function(e){
 
 
 //FUNCION ACTUALIZAR
-//FUNCION CREAR
 function actualizarPelicula(obj){
     $.ajax({
         method: 'PUT',
@@ -64,9 +63,50 @@ $("#BTN_ACTUALIZAR").click(function(){
     var Duracion = $("#Duracion").val();
     var Idiomas = $("#Idiomas").val();
     var Descripcion = $("#Descripcion").val();
-    var obj= {id:id,Nombre:Nombre,Genero:Genero,Año:Año,Productora:Productora,Pais:Pais,Duracion:Duracion,Idiomas:Idiomas,Descripcion:Descripcion};
+    var obj= {id:id,Nombre:Nombre,Genero:Genero,Anio:Año,Productora:Productora,Pais:Pais,Duracion:Duracion,Idiomas:Idiomas,Descripcion:Descripcion};
     actualizarPelicula(obj);
-
-
-
 })
+
+
+//FUNCION Borrar
+function borrarPelicula(obj){
+    $.ajax({
+        method: 'DELETE',
+        url: '/cgi-bin/Proyecto_arquitectura/App.py',
+        data: obj,
+        dataType: "json",
+        success: function(response) {
+            console.log(response)
+        },
+        error: function(response){
+            console.log("Error al eliminar pelicula")
+            console.log(JSON.stringify(response))
+
+        }
+    }); 
+}
+
+
+$("#BTN_ELIMINAR").click(function(){
+    e.preventDefault();
+    var id = $("#id").val();
+    var Nombre = $("#nombre").val();
+    var Genero = $("#genero").val();
+    var Año = $("#Año").val();
+    var Productora = $("#Productora").val();
+    var Pais = $("#Pais").val();
+    var Duracion = $("#Duracion").val();
+    var Idiomas = $("#Idiomas").val();
+    var Descripcion = $("#Descripcion").val();
+    var obj= {id:id,Nombre:Nombre,Genero:Genero,Anio:Año,Productora:Productora,Pais:Pais,Duracion:Duracion,Idiomas:Idiomas,Descripcion:Descripcion};
+    borrarPelicula(obj);
+})
+
+
+
+
+
+
+
+
+
