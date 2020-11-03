0d2d381985c4d45e3aa5d74e8c3df7499955654a
Natalia <montenegro.natismontenegro@hotmail.com>
2020-11-03 10:42:23 -0600

    arreglo año

--- js/Scrip.js
+++ js/Scrip.js
@@ -21,13 +21,13 @@ $("#BTN_CREAR").click(function(e){
     var id = $("#id").val();
     var Nombre = $("#nombre").val();
     var Genero = $("#genero").val();
-    var Año = $("#Año").val();
+    var Año = $("#Anio").val();
     var Productora = $("#Productora").val();
     var Pais = $("#Pais").val();
     var Duracion = $("#Duracion").val();
     var Idiomas = $("#Idiomas").val();
     var Descripcion = $("#Descripcion").val();
-    var obj= {id:id,Nombre:Nombre,Genero:Genero,Año:Año,Productora:Productora,Pais:Pais,Duracion:Duracion,Idiomas:Idiomas,Descripcion:Descripcion};
+    var obj= {id:id,Nombre:Nombre,Genero:Genero,Anio:Año,Productora:Productora,Pais:Pais,Duracion:Duracion,Idiomas:Idiomas,Descripcion:Descripcion};
     crearPelicula(obj);
 })
 
